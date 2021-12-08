604832f76eda150ea7b622ffa45ddcc7d9410891
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-12-08 12:02:46 +0100

    Added framework into index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php
 use Slim\Factory\AppFactory;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'loginapp',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
